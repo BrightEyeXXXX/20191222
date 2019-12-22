94bc0ff1db5f27fc5e10c2fee8895318ad4654ba
BrightEyeXXXX <miura.takashi0722@gmail.com>
2019-12-22 10:02:14 +0900

    Initial Commit

---
+++ 20191222/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  20191222
+//
+//  Created by 三浦隆 on 2019/12/22.
+//  Copyright © 2019 miura takashi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
