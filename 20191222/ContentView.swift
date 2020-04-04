885a40d2dcaf34cc4b7e2a80474e8c49581ac8ab
BrightEyeXXXX <miura.takashi0722@gmail.com>
2020-04-04 11:01:00 +0900

    Test connect github by xcode.

--- 20191222/ContentView.swift
+++ 20191222/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  20191222
 //
-//  Created by 三浦隆 on 2019/12/22.
+//  Created by tm on 2019/12/22.
 //  Copyright © 2019 miura takashi. All rights reserved.
 //
 
@@ -10,12 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello, World!_minor change")
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
